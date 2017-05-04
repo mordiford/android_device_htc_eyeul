ccf384d86a684b47acff26ca89967ba661247a72
tarkzim <tom8476oo@gmail.com>
2017-05-04 19:36:43 +0800

    eyeul: fix build path again

--- Android.mk
+++ Android.mk
@@ -14,10 +14,11 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := $(call my-dir)
+
 ifeq ($(TARGET_DEVICE),eyeul)
 
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 

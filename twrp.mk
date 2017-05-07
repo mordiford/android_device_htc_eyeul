3af94c2474fc91a4b4dd887efb22e2fb0e59c996
tarkzim <tom8476oo@gmail.com>
2017-05-07 18:41:06 +0800

    Revert "eyeul: fix fstab for TWRP"
    This reverts commit ff49f3e94168e030cc0a9789a9c942f3f3f3a2d9.

--- twrp.mk
+++ twrp.mk
@@ -3,7 +3,6 @@ PRODUCT_COPY_FILES += device/htc/eyeul/recovery/twrp.fstab:recovery/root/etc/twr
 RECOVERY_VARIANT := twrp
 
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TARGET_RECOVERY_FSTAB := device/htc/eyeul/recovery/twrp.fstab
 TW_IGNORE_MAJOR_AXIS_0 := true
 TW_MAX_BRIGHTNESS := 255
 TW_NO_SCREEN_BLANK := true
